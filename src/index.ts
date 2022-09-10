7ca1e23329aff5822ae55854425536185c4c6d8a
Mario Brusarosco <mariobrusarosco@hotmail.com>
2022-09-10 09:57:44 -0300

    Playground features: sending files + starting the parser and template engines

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,51 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import middleware1 from './middlewares/middleware1'
 import middleware2 from './middlewares/middleware2'
+
+import TournamentRouting from './domains/tournaments/routes/index'
+import logger from './middlewares/logger'
+import db from './db'
+import FileRouting from './playground/sending-files'
+import ServingWebsites from './playground/serving-sites'
 
 const PORT = 9090
 
 const app = express()
 
+db.connect()
+mongoose.connection.once('open', () => {
+  console.log('Mongo DB connection ready!')
+})
+
+mongoose.connection.on('error', err => {
+  console.error('error ----- ', err)
+})
+
 // JSON Parser Middleware
 app.use(express.json())
 
+app.use(logger)
+
 // Middleware flow POC!
-app.use(middleware1)
-app.use(middleware2)
-app.get('/middlewares', (_, res) => {
-  console.log('GET on /middlewares')
-  res.send('middlewares')
-})
+// app.use(middleware1)
+// app.use(middleware2)
+// app.get('/middlewares', (_, res) => {
+//   console.log('GET on /middlewares')
+//   res.send('middlewares')
+// })
 // Middleware flow POC!
 
 // Rest routes - temporary place
-app.get('/api/v1/tournaments', (req, res) => {
-  res.send([])
-})
+TournamentRouting(app)
+// tournamentRouter(app)
 
-app.post('/api/v1/tournaments', (req, res) => {
-  const body = req?.body
-
-  if (!body.label) {
-    res.status(400).json({ message: 'You must provide a label for a tournament' })
-  }
-
-  res.json(body)
-})
+// Rest routes - temporary place'
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
 console.log('best shot api!')
+
+// Playground Area
+FileRouting(app)
+ServingWebsites(app)

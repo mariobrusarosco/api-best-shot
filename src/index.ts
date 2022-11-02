825976df36e7e8d7f5f6943123f31ef4e69bd42a
Mario Brusarosco <mariobrusarosco@hotmail.com>
2022-11-02 08:43:23 -0300

    feat: allow origin

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,17 @@ const app = express()
 // JSON Parser Middleware
 app.use(express.json())
 app.use(logger)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin'
+  )
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+
+  next()
+})
 
 // Rest routes - temporary place
 TournamentRouting(app)

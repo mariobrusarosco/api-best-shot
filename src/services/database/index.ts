dc4ae1d8e95127892cd53d658b6339127a2dc2b0
mario <mariobrusarosco@hotmail.com>
2024-09-30 21:04:51 -0300

    chore(General): updating Docs with the updated local setup

--- src/services/database/index.ts
+++ src/services/database/index.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
-const connectionString = process.env.DATABASE_URL as string
+const connectionString = process.env.DB_CREDENTIALS as string
 
 const client = postgres(connectionString)
 const db = drizzle(client)

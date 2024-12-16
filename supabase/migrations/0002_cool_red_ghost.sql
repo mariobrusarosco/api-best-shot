bd5384e572dfd6115d7e6cbf62111fcb9d5573cc
mario <mariobrusarosco@hotmail.com>
2024-12-16 08:40:52 -0300

    fix: Team Schema with primary key: id and provider

--- supabase/migrations/0002_cool_red_ghost.sql
+++
@@ -1,5 +0,0 @@
-DO $$ BEGIN
- ALTER TABLE "guess" ADD CONSTRAINT "guess_match_id_match_id_fk" FOREIGN KEY ("match_id") REFERENCES "public"."match"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
--- supabase/migrations/0003_parallel_radioactive_man.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "guess" DROP CONSTRAINT "guess_match_id_match_id_fk";

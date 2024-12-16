217b3c695f03cc76d11295c78560fb03ccf40c73
mario <mariobrusarosco@hotmail.com>
2024-12-16 12:23:24 -0300

    update tournament ranking

--- src/domains/performance/database/index.ts
+++ src/domains/performance/database/index.ts
@@ -1,3 +1,31 @@
-const getTournament;
+import { runGuessAnalysis } from '@/domains/guess/controllers/guess-analysis';
+import { T_Guess } from '@/domains/guess/schema';
+import { GuessUtils } from '@/domains/guess/utils';
+import { T_Match } from '@/domains/match/schema';
+import db from '@/services/database';
+import { and, eq } from 'drizzle-orm';
 
-export const Database = {};
+const queryTournamentPerformance = async (memberId: string, tournamentId: string) => {
+  const guesses = await db
+    .select()
+    .from(T_Guess)
+    .innerJoin(T_Match, eq(T_Match.id, T_Guess.matchId))
+    .where(and(eq(T_Guess.memberId, memberId), eq(T_Match.tournamentId, tournamentId)));
+
+  const parsedGuesses = guesses.map(row => runGuessAnalysis(row.guess, row.match));
+
+  //@ts-ignore
+  const guessesByStatus = Object.groupBy(parsedGuesses, ({ status }) => status);
+  const guessesByStatusQty = (
+    Object.entries(guessesByStatus) as [string, any[]][]
+  ).reduce((acc, [key, body]) => ({ ...acc, [key]: body?.length }), {});
+
+  return {
+    details: guessesByStatusQty,
+    points: GuessUtils.getTotalPoints(parsedGuesses),
+  };
+};
+
+export const DB_Performance = {
+  queryTournamentPerformance,
+};

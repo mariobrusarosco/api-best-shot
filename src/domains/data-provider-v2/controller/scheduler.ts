d6cc909a74250f0563648abe7a4c8db18486bbec
mario <mariobrusarosco@hotmail.com>
2024-12-21 20:01:42 -0300

    feat(Schedule): using Schedules instead of rules

--- src/domains/data-provider-v2/controller/scheduler.ts
+++ src/domains/data-provider-v2/controller/scheduler.ts
@@ -2,25 +2,18 @@ import { T_Match } from '@/domains/match/schema';
 import { handleInternalServerErrorResponse } from '@/domains/shared/error-handling/httpResponsesHelper';
 import { T_Tournament } from '@/domains/tournament/schema';
 import db from '@/services/database';
-import {
-  EventBridgeClient,
-  PutRuleCommand,
-  PutRuleCommandInput,
-  PutTargetsCommand,
-  PutTargetsCommandInput,
-} from '@aws-sdk/client-eventbridge';
+import { CreateScheduleCommand, SchedulerClient } from '@aws-sdk/client-scheduler';
 import dayjs from 'dayjs';
 import isToday from 'dayjs/plugin/isToday';
 import utc from 'dayjs/plugin/utc';
 import { asc, eq, sql } from 'drizzle-orm';
 import { Request, Response } from 'express';
-
-const client = new EventBridgeClient({ region: 'us-east-1' });
-const DATA_PROVIDER_ROUNDS_URL = `${process.env.API_DOMAIN}/data-provider/tournament/[tournamentId]/round/[round]`;
-const DATA_PROVIDER_STANDINGS_URL = `${process.env.API_DOMAIN}/data-provider/tournaments/[tournamentId]/standings`;
-
 dayjs.extend(utc);
 dayjs.extend(isToday);
+
+const DATA_PROVIDER_ROUNDS_URL = `${process.env.API_DOMAIN}/data-provider/tournament/[tournamentId]/round/[round]`;
+const DATA_PROVIDER_STANDINGS_URL = `${process.env.API_DOMAIN}/data-provider/tournaments/[tournamentId]/standings`;
+const client = new SchedulerClient({ region: 'us-east-1' });
 
 const run = async (req: Request, res: Response) => {
   try {
@@ -28,7 +21,8 @@ const run = async (req: Request, res: Response) => {
     const dailySchedule = mapMatchesToDailySchedule(currentDayMatches);
 
     dailySchedule.forEach(async schedule => {
-      await scheduleMatchScoreUpdateCronJob(schedule);
+      await scheduleTournamentStandingsUpdateCronJob(schedule);
+      await scheduleTournamentRoundUpdateCronJob(schedule);
     });
 
     res.status(200).send(currentDayMatches);
@@ -37,22 +31,6 @@ const run = async (req: Request, res: Response) => {
 
     handleInternalServerErrorResponse(res, error);
   }
-};
-
-const toCronFormat = (date: ReturnType<typeof dayjs>) => {
-  const d = dayjs(date);
-  if (!d.isValid()) {
-    throw new Error('Invalid date');
-  }
-
-  const minutes = d.minute();
-  const hours = d.hour();
-  const dayOfMonth = d.date();
-  const month = d.month() + 1;
-  const dayOfWeek = '?';
-  const year = d.year();
-
-  return `cron(${minutes} ${hours} ${dayOfMonth} ${month} ${dayOfWeek} ${year})`;
 };
 
 const queryCurrentDayMatchesOnDatabase = async () => {
@@ -82,13 +60,12 @@ const mapMatchesToDailySchedule = (
 
   matches.forEach(({ tournamentId, tournamentLabel, round, date }) => {
     const utcDate = dayjs(date).utc();
-    const SCHEDULE_ID = `tourn_${tournamentLabel}_round_${round}_${utcDate.format(
-      'MM_DD_HH_mm_YYYY'
-    )}`;
+    const SCHEDULE_ID = `${tournamentLabel}_${utcDate.format('MM_DD_HH_mm_YYYY')}`;
 
     if (!SCHEDULES.has(SCHEDULE_ID)) {
       SCHEDULES.set(SCHEDULE_ID, {
         tournamentIdToUpdate: tournamentId,
+        tournamentLabelToUpdate: tournamentLabel,
         roundToUpdate: round ?? '',
         cron: toCronFormat(utcDate),
         id: SCHEDULE_ID,
@@ -99,48 +76,83 @@ const mapMatchesToDailySchedule = (
   return SCHEDULES;
 };
 
-const scheduleMatchScoreUpdateCronJob = async (schedule: ISchedule) => {
-  const ruleName = schedule.id;
+const toCronFormat = (date: ReturnType<typeof dayjs>) => {
+  const d = dayjs(date);
+  if (!d.isValid()) {
+    throw new Error('Invalid date');
+  }
 
-  const rule = {
-    Name: ruleName,
-    ScheduleExpression: schedule.cron,
-  } as PutRuleCommandInput;
+  const minutes = d.minute();
+  const hours = d.hour();
+  const dayOfMonth = d.date();
+  const month = d.month() + 1;
+  const dayOfWeek = '?';
+  const year = d.year();
 
-  const roundsUrl = DATA_PROVIDER_ROUNDS_URL.replace(
-    '[tournamentId]',
-    schedule.tournamentIdToUpdate
-  ).replace('[round]', schedule.roundToUpdate);
+  return `cron(${minutes} ${hours} ${dayOfMonth} ${month} ${dayOfWeek} ${year})`;
+};
+
+const scheduleTournamentStandingsUpdateCronJob = async (schedule: ISchedule) => {
   const standingsUrl = DATA_PROVIDER_STANDINGS_URL.replace(
     '[tournamentId]',
     schedule.tournamentIdToUpdate
   );
-  const ruleTarget = {
-    Rule: ruleName,
-    Targets: [
-      {
-        Id: ruleName,
-        Arn: 'arn:aws:lambda:us-east-1:905418297381:function:match-score-update',
-        Input: JSON.stringify({
-          roundsUrl,
-          standingsUrl,
-        }),
-      },
-    ],
-  } as PutTargetsCommandInput;
 
-  await client.send(new PutRuleCommand(rule));
-  console.log(`Rule created: ${ruleName}`);
+  const params = {
+    Name: `standings_${schedule.id}`,
+    ScheduleExpression: schedule.cron,
+    Target: {
+      Arn: 'arn:aws:lambda:us-east-1:905418297381:function:update-tournament-standings-demo',
+      RoleArn:
+        'arn:aws:iam::905418297381:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_b425d46da9',
+      Input: JSON.stringify({ standingsUrl }),
+    },
+    State: 'ENABLED' as const, // Schedule state: 'ENABLED' or 'DISABLED',
+    FlexibleTimeWindow: {
+      Mode: 'OFF' as const,
+    },
+    ActionAfterCompletion: undefined,
+    GroupName: 'tournament-standings',
+  };
 
-  await client.send(new PutTargetsCommand(ruleTarget));
-  console.log(`Target attached to rule: ${ruleName}`);
+  const command = new CreateScheduleCommand(params);
+  const response = await client.send(command);
+  console.log(`Schedule created: ${response.ScheduleArn}`);
 };
 
-type IDailySchedule = ISchedule[];
+const scheduleTournamentRoundUpdateCronJob = async (schedule: ISchedule) => {
+  const roundsUrl = DATA_PROVIDER_ROUNDS_URL.replace(
+    '[tournamentId]',
+    schedule.tournamentIdToUpdate
+  ).replace('[round]', schedule.roundToUpdate);
+
+  const params = {
+    Name: `round_${schedule.id}`,
+    ScheduleExpression: schedule.cron,
+    Target: {
+      Arn: 'arn:aws:lambda:us-east-1:905418297381:function:update-tournament-round-demo',
+      RoleArn:
+        'arn:aws:iam::905418297381:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_b425d46da9',
+      Input: JSON.stringify({ roundsUrl }),
+    },
+    State: 'ENABLED' as const,
+    FlexibleTimeWindow: {
+      Mode: 'OFF' as const,
+    },
+    ActionAfterCompletion: undefined,
+    GroupName: 'tournament-round',
+  };
+
+  const command = new CreateScheduleCommand(params);
+  const response = await client.send(command);
+  console.log(`Schedule created: ${response.ScheduleArn}`);
+};
+
 type ISchedule = {
   id: string;
   cron: string;
   tournamentIdToUpdate: string;
+  tournamentLabelToUpdate: string;
   roundToUpdate: string;
 };
 

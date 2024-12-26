42f31542aeb0708236bc5cd1a75c607745590e68
mario <mariobrusarosco@hotmail.com>
2024-12-26 16:06:49 -0300

    FEAT: /PATCH  for /teams - regular and regular+knockout season

--- src/domains/data-provider/controllers/teams/index.ts
+++ src/domains/data-provider/controllers/teams/index.ts
@@ -1,4 +1,4 @@
-import { SofascoreTeams } from '@/domains/data-provider-v2/providers/sofascore/sofacore-teams';
+import { SofascoreTeams } from '@/domains/data-provider/providers/sofascore/teams';
 import { TournamentQueries } from '@/domains/tournament/queries';
 import {
   fetchTeamForRegularAndKnockoutTournament,
@@ -16,7 +16,6 @@ const create = async (tournamentId: string) => {
     );
 
     const tournamentMode = tournament.mode;
-
     if (tournamentMode === 'regular-season-and-knockout') {
       const teamsToInsert = await fetchTeamForRegularAndKnockoutTournament(tournament);
 
@@ -26,7 +25,7 @@ const create = async (tournamentId: string) => {
     if (tournamentMode === 'regular-season-only') {
       const teamsToInsert = await fetchTeamsForRegularSeason(tournament);
 
-      await SofascoreTeams.createOnDatabase(teamsToInsert);
+      return await SofascoreTeams.createOnDatabase(teamsToInsert);
     }
 
     return [];
@@ -37,6 +36,24 @@ const update = async (tournamentId: string) => {
   try {
     const tournament = await TournamentQueries.tournament(tournamentId);
     if (tournament === undefined) throw new Error('Tournament not found');
+
+    console.log(
+      '[LOG] - [TeamsController] - CREATING TOURNAMENT ROUNDS FOR: ',
+      tournament.label
+    );
+
+    const tournamentMode = tournament.mode;
+    if (tournamentMode === 'regular-season-and-knockout') {
+      const teamsToInsert = await fetchTeamForRegularAndKnockoutTournament(tournament);
+
+      return await SofascoreTeams.upsertOnDatabase(teamsToInsert);
+    }
+
+    if (tournamentMode === 'regular-season-only') {
+      const teamsToInsert = await fetchTeamsForRegularSeason(tournament);
+
+      await SofascoreTeams.upsertOnDatabase(teamsToInsert);
+    }
 
     return [];
   } catch (error: any) {

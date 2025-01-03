32d8ba977db3d29386cc8ec5400b802c5bf42957
mario <mariobrusarosco@hotmail.com>
2025-01-03 10:29:04 -0300

    Fix: Logging for tournament setup data-provider

--- src/domains/data-provider/api/tournament/index.ts
+++ src/domains/data-provider/api/tournament/index.ts
@@ -1,6 +1,7 @@
 import { TournamentRequest } from '@/domains/data-provider/api/tournament/typing';
 import { TournamentController } from '@/domains/data-provider/controllers/tournament/tournament';
 import { handleInternalServerErrorResponse } from '@/domains/shared/error-handling/httpResponsesHelper';
+import Profiling from '@/services/profiling';
 import { Response } from 'express';
 import { MatchesController } from '../../controllers/matches';
 import { StandingsController } from '../../controllers/standings';
@@ -16,17 +17,33 @@ const setup = async (req: TournamentRequest, res: Response) => {
     if (!newTournament)
       throw new Error('[ERROR] - [DATA PROVIDER] - [SETUP] - Tournament not created');
 
+    Profiling.log('[DATA PROVIDER] - [SETUP] - [STEP 1]', {
+      tournament: newTournament,
+    });
+
     // SETUP TOURNAMENTS ROUNDS
     const newRounds = await TournamentRoundController.create(newTournament.id!);
+    Profiling.log('[DATA PROVIDER] - [SETUP] - [STEP 2]', {
+      rounds: newRounds,
+    });
 
     // SETUP TEAMS
     const teams = await TeamsController.create(newTournament.id!);
+    Profiling.log('[DATA PROVIDER] - [SETUP] - [STEP 3]', {
+      teams,
+    });
 
     // SETUP MATCHES
     const matches = await MatchesController.create(newTournament.id!);
+    Profiling.log('[DATA PROVIDER] - [SETUP] - [STEP 4]', {
+      matches,
+    });
 
     // SETUP STANDINGS
     const standings = await StandingsController.create(newTournament.id!);
+    Profiling.log('[DATA PROVIDER] - [SETUP] - [STEP 5]', {
+      standings,
+    });
 
     return res.status(200).send('[SETUP COMPLETED]');
   } catch (error: any) {

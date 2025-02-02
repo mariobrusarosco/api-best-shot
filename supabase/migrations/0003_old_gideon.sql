6c10d954c3665b65a84e891c2b2e17226c90c8b2
mario <mariobrusarosco@hotmail.com>
2025-02-02 16:59:23 -0300

    Removing migrations

--- supabase/migrations/0003_old_gideon.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "guess" ADD COLUMN "active" boolean DEFAULT true NOT NULL;
--- supabase/migrations/0004_spicy_triathlon.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "tournament_performance" RENAME COLUMN "tournamentId_id" TO "tournament_id";--> statement-breakpoint
-DROP INDEX IF EXISTS "unique_tournament_perfomance";--> statement-breakpoint
-ALTER TABLE "tournament_performance" DROP CONSTRAINT "tournament_performance_member_id_tournamentId_id_pk";--> statement-breakpoint
-ALTER TABLE "tournament_performance" ADD CONSTRAINT "tournament_performance_member_id_tournament_id_pk" PRIMARY KEY("member_id","tournament_id");--> statement-breakpoint
-CREATE UNIQUE INDEX IF NOT EXISTS "unique_tournament_perfomance" ON "tournament_performance" USING btree ("member_id","tournament_id");

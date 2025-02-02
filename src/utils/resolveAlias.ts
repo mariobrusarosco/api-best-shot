1beef5b6bfd3d274cb08f609f01020ee9472d5c4
mario <mariobrusarosco@hotmail.com>
2025-02-01 21:23:16 -0300

    FIX

---
+++ src/utils/resolveAlias.ts
@@ -0,0 +1,8 @@
+import path from "path";
+
+export function resolveAlias(aliasPath: string): string {
+    if (aliasPath.startsWith("@/")) {
+        return path.join(__dirname, "../", aliasPath.replace("@/", ""));
+    }
+    return aliasPath;
+} 

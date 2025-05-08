c5d0a4c487b0b0eab6e7edf9b63152682e0cee7e
Mario Brusarosco <mariobrusarosco@hotmail.com>
2025-05-08 10:43:42 -0300

    Removing unused util methods

--- src/domains/tournament/utils.ts
+++
@@ -1,6 +0,0 @@
-import db from '@/services/database';
-import { eq } from 'drizzle-orm';
-import { T_Tournament } from './schema';
-
-export const getTournamentById = async (id: string) =>
-  db.select().from(T_Tournament).where(eq(T_Tournament.id, id));

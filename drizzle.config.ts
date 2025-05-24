96cb21331f76340f5979ad79878916d850002b2b
Mario Brusarosco <mariobrusarosco@hotmail.com>
2025-05-24 19:51:28 -0300

    DEMO MIgrations with yarn

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
-import { env } from './src/config/env';
 
 export default defineConfig({
   schema: './src/services/database/schema.ts',
   out: './supabase/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DB_STRING_CONNECTION,
+    url: process.env.DB_STRING_CONNECTION || '',
   },
 });

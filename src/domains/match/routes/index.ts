22eec2575ecadf1cd5ccff6760682a3c343f2114
Mario Brusarosco <mariobrusarosco@hotmail.com>
2025-06-28 11:01:23 -0300

    Fix: Routing Improvement: Match Domain

--- src/domains/match/routes/index.ts
+++ src/domains/match/routes/index.ts
@@ -1,13 +1,2 @@
-import express from 'express';
-import ApplicationRouter from '@/router';
-import { AuthMiddleware } from '@/domains/auth/middleware';
-
-const RouterV1 = express.Router();
-RouterV1.use(AuthMiddleware);
-
-ApplicationRouter.register('api/v1/match', RouterV1);
-
-const RouterV2 = express.Router();
-RouterV2.use(AuthMiddleware);
-
-ApplicationRouter.register('api/v2/match', RouterV2);
+export { default as v1 } from './v1';
+export { default as v2 } from './v2';
